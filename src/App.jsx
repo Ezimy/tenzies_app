e1612a0c658b5067cd9774472ac9806258f29eb6
Sammy005 <98131603+Ezimy@users.noreply.github.com>
2024-01-28 20:03:55 -0800

    css random number

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
 import React from 'react'
 import Die from '/src/components/Die.jsx'
 export default function App() {
-function dieRoll(){
-    return(Math.floor(Math.random() * 6) + 1)
+  const [diceArray, setDiceArray] = React.useState(allNewDice)
+  function allNewDice() {
+    const newDice = []
+    for (let i = 0; i < 10; i++) {
+        newDice.push(Math.ceil(Math.random() * 6))
+    }
+    return newDice
 }
+function rollDice() {
+  setDiceArray(allNewDice)
+}
+const diceElements = diceArray.map(die => <Die value={die}/>)
+console.log(allNewDice())
   return (
+    <>
     <main>
-        <Die value={dieRoll()}/>
-        <Die value={dieRoll()}/>
-        <Die value={dieRoll()}/>
-        <Die value={dieRoll()}/>
-        <Die value={dieRoll()}/>
-        <Die value={dieRoll()}/>
-        <Die value={dieRoll()}/>
-        <Die value={dieRoll()}/>
-        <Die value={dieRoll()}/>
-        <Die value={dieRoll()}/>
+      {diceElements}
     </main>
+    <button className='roll-dice' onClick={rollDice}>Roll</button>
+    </>
   )
 }
