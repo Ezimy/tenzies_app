a3ff6ef2f65bbc97e3622ef47eb5755494a9326e
Sammy005 <98131603+Ezimy@users.noreply.github.com>
2024-01-29 20:56:53 -0800

    changed dice numbers to dots and added styling

--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -3,9 +3,20 @@ export default function Die(props){
     const styles = {
         backgroundColor: props.isHeld ? "#59E391" : "white"
     }
+    function generateDotDiv(){
+        return <div className="dot"></div>
+    }
+    function dots() {
+        const dotsArray = []
+        for(let i = 0 ; i < props.value; i++){
+            dotsArray.push(generateDotDiv())
+        }
+        console.log(dotsArray)
+        return dotsArray
+    }
     return(
-        <div className="die" style={styles} onClick={props.holdDice}>
-            <h2>{props.value}</h2>
+        <div className={`die die-${props.value}`} style={styles} onClick={props.holdDice}>
+            {dots()}
         </div>
     )
 }
