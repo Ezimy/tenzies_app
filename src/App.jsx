f69fa450fffd36ba6a84be89fb4ee41fa2ccc40a
Sammy005 <98131603+Ezimy@users.noreply.github.com>
2024-01-29 17:17:32 -0800

    fixed confetti not spanning whole page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react"
+import React from "react"
 import Confetti from "react-confetti"
 import Die from "/src/components/Die"
 import {nanoid} from "nanoid"
@@ -74,7 +74,9 @@ export default function App() {
     
     return (
         <main>
-            {tenzies && <Confetti />}
+            {tenzies && <Confetti
+            width={window.innerWidth}
+            height={window.innerHeight}/>}
             <h1 className="title">Tenzies</h1>
             <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
             <div className="dice-container">
